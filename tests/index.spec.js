c695ffd9fc576a9a59dda7693169d3dca318dadd
paiizz <wanchanapon.t@ku.th>
2018-09-06 13:01:26 +0700

    add test file

---
+++ tests/index.spec.js
@@ -0,0 +1,14 @@
+const server = require('../index.js')
+// const request = require('supertest')
+afterEach(() => {
+  console.log('-')
+  server.close()
+})
+describe('routes: index', () => {
+  test('should response as expected', async () => {
+    const response = await request(server).get('/')
+    expect(response.status).toEqual(200)
+    expect(response.type).toEqual('application/json')
+    expect(response.body.msg).toEqual('Hello World')
+  })
+})
